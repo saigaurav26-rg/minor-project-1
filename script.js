18cefc720bf2471c40df3286a25358b2f61e7622
Sai Gaurav <Saigaurav265@gmail.com>
2026-04-27 13:34:51 +0530

    new logic added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/// function openfeature() represents the opening of perticular car like todo, daily planner ,etc
+
 function openfeatures() {
     let allElem = document.querySelectorAll(".elem");
 let FullElemPage = document.querySelectorAll(".full-elem");
@@ -6,10 +8,13 @@ let FullElemPagebackbtn = document.querySelectorAll(".back");
 allElem.forEach(function (elem) {
     //console.log(elem.id);
     elem.addEventListener("click",function () {
-         FullElemPage.forEach(function(full) {
-            full.style.display = "none";
+         FullElemPage.forEach(function() {
+            //full.style.display = "none";
+            FullElemPage[elem.id].style.display = "flex";
+           
         })
-        FullElemPage[elem.id].style.display = "flex";
+         
+        
 
        FullElemPagebackbtn.forEach(function(back) {
             back.addEventListener("click",function(){
@@ -21,3 +26,72 @@ allElem.forEach(function (elem) {
 })
 }
 openfeatures();
+
+let form = document.querySelector("form");
+let taskinput = document.querySelector("form input");
+let taskinputtextarea = document.querySelector("form textarea");
+let taskcheckbox = document.querySelector("form #check");
+
+
+let currenttask = [
+    {
+        task : "ghar jao",
+        details :"padai karo",
+        imp: true
+    },
+
+    {
+        task : "revison ",
+        details :"coading practice",
+        imp: true
+    },
+
+    {
+        task : "phone chalana",
+        details :"raat ko sote samay",
+        imp: false
+    },
+]
+
+// rendertask() function is used to add all the task present in current task into alltasks
+function rendertask(){
+    let allTask = document.querySelector(".all-task");
+
+var sum = "";
+
+currenttask.forEach(function(elem){
+    sum= sum + `<div class="tasks">
+                        <h5>${elem.task}<span class=${elem.imp}>imp</span></h5>
+                        <button>mark as completed</button>
+                    </div>`
+    
+});
+
+allTask.innerHTML = sum;
+}
+rendertask();
+
+// here we are push the input we are giving into alltask using render funtion...
+form.addEventListener("submit",function(dets){
+    dets.preventDefault();////stops the default behavior of form i.e.stops reloading the page//////
+
+    //console.log(taskinput.value);
+    //console.log(taskinputtextarea.value);
+    //console.log(taskcheckbox.checked);
+    
+    currenttask.push(
+        {
+        task: taskinput.value,
+        details: taskinputtextarea.value,
+        imp: taskcheckbox.checked,
+    }
+)
+ taskinput.value="";
+ taskinputtextarea.value="";
+ taskcheckbox.checked= false;
+
+rendertask();
+
+    
+});
+
